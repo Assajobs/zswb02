a54f39268fb20a995f522107e84e80f9468670f0
assajobs <nqjack@aliyun.com>
2020-05-13 12:06:15 +0800

    zswb02_first

---
+++ src/main/java/com/example/zswb02/Zswb02Application.java
@@ -0,0 +1,13 @@
+package com.example.zswb02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zswb02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zswb02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zswb02/Zswb02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zswb02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zswb02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
