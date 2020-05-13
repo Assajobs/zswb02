0c524a3e225dba8747ff25a7b4a224da24804f80
assajobs <nqjack@aliyun.com>
2020-05-13 13:42:06 +0800

    zswb02 添加dao service controller包

--- src/main/java/com/example/zswb02/controller/PulishRequireController.java
+++ src/main/java/com/example/zswb02/controller/PulishRequireController.java
@@ -1,4 +1,11 @@
 package com.example.zswb02.controller;
 
+import com.example.zswb02.dao.UsersMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class PulishRequireController {
+
+
 }
